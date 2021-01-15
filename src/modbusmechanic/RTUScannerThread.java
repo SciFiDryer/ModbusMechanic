2b9d6d042ced24568837fde045ef896e4b9bfb99
Matt Jamesson <scifidryer@gmail.com>
2021-01-15 18:29:01 -0500

    RTU debug messages cleaned up.

--- src/modbusmechanic/RTUScannerThread.java
+++ src/modbusmechanic/RTUScannerThread.java
@@ -89,7 +89,10 @@ public class RTUScannerThread extends Thread {
         }
         catch (Exception e)
         {
-            e.printStackTrace();
+            if (ModbusMechanic.debug)
+            {
+                e.printStackTrace();
+            }
         }
         parentFrame.repaint();
     }
